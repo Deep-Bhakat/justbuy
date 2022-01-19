25b39ea238a5c013a546ec98062076b2bb1c6a0d
Deep-Bhakat <deepbhakat234@gmail.com>
2022-01-19 20:41:17 +0530

    set up error handling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDatabase = require('./database');
 const productRoutes = require('./routes/productRoute');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 const app = express()
 
 //setting up config file
@@ -14,6 +15,19 @@ app.use(express.json());
 //setting the routes
 app.use('/api',productRoutes);
 
-app.listen(process.env.PORT, ()=>{
+//setting up error handling
+app.use(errorHandlerMiddleware);
+
+const server = app.listen(process.env.PORT, ()=>{
     console.log(`Started server at PORT ${process.env.PORT}`);
 });
+
+
+//handle unhandled promise rejection
+process.on('unhandledRejection', err =>{
+    console.log(`Error : ${err.message}`);
+    console.log('Shutting down the server due to unhandled promise rejection');
+    server.close(()=>{
+        process.exit(1);
+    });
+})
