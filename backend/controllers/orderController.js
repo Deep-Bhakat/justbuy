9d292472d0a5c3d9d472f0b4794a079a7ba278af
Deep-Bhakat <deepbhakat234@gmail.com>
2022-01-28 20:13:03 +0530

    added update order and delete order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -87,3 +87,60 @@ exports.allOrders = async(req,res,next) =>{
         message:'Orders fetched successfully.'
     });
 };
+
+exports.updateOrder = async(req,res,next) =>{
+    const order = await Order.findById(req.params.orderId);
+
+    if(!order){
+        return next(new ErrorHandler('Order not found',500));      
+    }
+    if(order.orderStatus === 'Delivered'){
+        return next(new ErrorHandler('You have already delivered the order!',400));       
+    }
+
+    order.orderItems.forEach(async item =>{
+        await updateStock(item.product,item.quantity);
+    });
+    
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now();
+
+    await order.save();
+
+    res.status(200).json({
+        success: true,   
+        message:'Order status updated successfully.'
+    });
+};
+
+exports.deleteOrder = async (req,res,next) =>{
+
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if(!order){
+        return next(new ErrorHandler('Order not found',500));  
+    }
+
+    try{
+        await Order.findByIdAndDelete(orderId);
+
+        res.status(200).json({
+            success:true,
+            message:'Order deleted successfully'
+        });
+    }catch(err){
+        return next(new ErrorHandler(err.message,500));        
+    }
+};
+
+
+async function updateStock(id,qty){
+    const product = await Product.findById(id);
+
+    if(!product){
+        return next(new ErrorHandler('Product not found',500));  
+    }
+    product.stock-= qty;
+    await product.save({validateBeforeSave: false});
+};
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -8,5 +8,7 @@ router.post('/order/new', isAuthenticated, orderController.createOrder);
 router.get('/order/:orderId', isAuthenticated, orderController.getOrder);
 router.get('/orders', isAuthenticated, orderController.myOrders);
 router.get('/admin/orders', isAuthenticated, authorizeRole('admin'), orderController.allOrders);
+router.put('/admin/order/:orderId', isAuthenticated, authorizeRole('admin'), orderController.updateOrder);
+router.delete('/admin/order/:orderId', isAuthenticated, authorizeRole('admin'), orderController.deleteOrder);
 
 module.exports = router;
