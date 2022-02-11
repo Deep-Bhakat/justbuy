54f9be43ac573a0cd0e03e8b2ab1f8f5e29f7f2f
Deep-Bhakat <deepbhakat234@gmail.com>
2022-02-11 22:41:45 +0530

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import Home from './component/Home';
 import Footer from './component/layout/Footer';
 import Header from './component/layout/Header';
-
+import {BrowserRouter as Router,Route, Routes, Switch} from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header />
-      <Home />
+    <Router>
+    <Header />
+      <Routes>     
+      <Route path='/' element={<Home />} exact />
+      </Routes>
       <Footer />
+      </Router>
     </>
   );
 }
